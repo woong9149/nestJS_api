5102565ba138000ccfaa977e9b9c86b7192b5ea4
woong9149 <sewoong0409@naver.com>
2021-03-27 20:27:17 +0900

    210327_2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Controller, Module } from '@nestjs/common';
-import { MoviesController } from './movies/movies.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
-import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [
@@ -19,8 +19,9 @@ import { MoviesService } from './movies/movies.service';
       synchronize: true,
     }),
     UserModule,
+    MoviesModule,
   ],
-  controllers: [MoviesController],
-  providers: [MoviesService],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -33,7 +34,7 @@ export class MoviesController {
 	}
 
 	@Patch('/:id')
-	path(@Param('id') movieId: number, @Body() updateData) {
+	path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
 		return this.moviesService.update(movieId, updateData);
 	}
 
