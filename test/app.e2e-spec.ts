65097f0fc4e7ed7581168426a5613044e0a1ca0d
woong9149 <sewoong0409@naver.com>
2021-04-02 22:00:31 +0900

    210402

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,25 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
   });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+      .get('/movies')
+      .expect(200)
+      .expect([]);
+    })
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+      .post('/movies')
+      .send({
+        title: 'Test Title',
+        year : '2021',
+        genres : ['Test']
+      })
+    })
+  })
 });
