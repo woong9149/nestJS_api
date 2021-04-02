2d5b68ba62f14437a30fb00bcae903e2c3785ff7
woong9149 <sewoong0409@naver.com>
2021-04-02 21:34:20 +0900

    210402

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,21 @@
 import { Controller, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
-import { UserModule } from './user/user.module';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'boa457813',
-      database: 'opentutorials',
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
-    UserModule,
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'boa457813',
+    //   database: 'opentutorials',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    // }),
+    // UserModule,
     MoviesModule,
   ],
   controllers: [AppController],
